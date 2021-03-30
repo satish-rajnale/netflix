dd1c98654ef8b58a17278f39db092e0f44147a60
satish-rajnale <satishrajnale98@gmail.com>
2021-03-30 17:47:53 +0530

    feature component added

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,11 +6,23 @@ import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import HeaderContainer from '../containers/header';
 import {JumbotronContainer} from '../containers/jumbotron';
-
+import { OptForm } from '../components';
+import { Feature } from '../components';
 export default function Home(){
     return(
         <>
         <HeaderContainer>
+            <Feature>
+                <Feature.Title>Unlimited movies, TV shows and more.</Feature.Title>
+                <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+            </Feature>
+            <OptForm> 
+                <OptForm.Text>
+                Ready to watch? Enter your email to create or restart your membership.
+                </OptForm.Text>
+                <OptForm.Input placeholder="Email Address"/>
+                <OptForm.Button>Get Started</OptForm.Button>
+            </OptForm>
         </HeaderContainer>
             <JumbotronContainer/>
             <FaqsContainer/>
