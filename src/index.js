197631ecaff5da35b3a0b367d3accdbcd0b0c11a
satish-rajnale <satishrajnale98@gmail.com>
2021-04-11 15:34:10 +0530

    react srtictmode and hashrouter

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import {render} from 'react-dom';
 import {GlobalStyles} from "./global-styles";
 import 'normalize.css';
 import App from './App';
+import {HashRouter} from 'react-router-dom';
 import { firebase } from './lib/firebase.prod';
 import {FirebaseContext} from './context/firebase';
 
 
 render(
-    <>
+    <React.StrictMode>
+        <HashRouter>
         <FirebaseContext.Provider value={{ firebase}}>
             <GlobalStyles/> 
             <App />
         </FirebaseContext.Provider>
-    </>,
+        </HashRouter>
+        </React.StrictMode>
+    ,
     document.getElementById('root')
 );
 
