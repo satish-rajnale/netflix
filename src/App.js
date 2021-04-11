fc21bd9fdc41d67c276018bf1553c8ec83eb794b
satish-rajnale <satishrajnale98@gmail.com>
2021-04-11 15:12:02 +0530

    gh pages fixed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
 
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
-import {IsUserRedirect, ProtectedRoute} from './helpers/routes';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import * as ROUTES from './constants/routes';
-import {Home, Browse, SignIn, SignUp} from './pages';
-import { useAuthListener} from './hooks';
+import { Home, Browse, SignIn, SignUp } from './pages';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = useAuthListener();
+   const user = useAuthListener();
 
-  return (
-   <Router>
-     <Switch>
-        {/* <IsUserRedirect
-          user={user}
-          loggedInPath={ROUTES.BROWSE}
-          path={ROUTES.SIGN_IN}>
-            <SignIn/>
-        </IsUserRedirect>
-        <IsUserRedirect
-          user={user}
-          loggedInPath={ROUTES.BROWSE}
-          path={ROUTES.SIGN_UP}>
-            <SignUp/>
-        </IsUserRedirect>
-       <ProtectedRoute user={user} path={ROUTES.BROWSE} >
-          <Browse/>
-       </ProtectedRoute>
-    
-         <ProtectedRoute user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
-           <Home/>
-        </ProtectedRoute> */}
+   return (
+      <Router>
+         <Switch>
+            <IsUserRedirect
+               user={user}
+               loggedInPath={ROUTES.BROWSE}
+               path={ROUTES.SIGN_IN}>
+               <SignIn />
+            </IsUserRedirect>
+            <IsUserRedirect
+               user={user}
+               loggedInPath={ROUTES.BROWSE}
+               path={ROUTES.SIGN_UP}>
+               <SignUp />
+            </IsUserRedirect>
+            <ProtectedRoute user={user} path={ROUTES.BROWSE} >
+               <Browse />
+            </ProtectedRoute>
 
-<Route path={ROUTES.HOME} exact>
-       <Home/>
-     </Route>
-     <Route path={ROUTES.SIGN_IN} >
-        <SignIn/>
-     </Route>
-     <Route path={ROUTES.BROWSE} >
-        <Browse/>
-     </Route>
-     <Route path={ROUTES.SIGN_UP} >
-        <SignUp/>
-     </Route>
-      </Switch>
-   </Router> 
-  );
+            <ProtectedRoute user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+               <Home />
+            </ProtectedRoute>
+
+            {/* <Route path={ROUTES.HOME} exact>
+               <Home />
+            </Route>
+            <Route path={ROUTES.SIGN_IN} >
+               <SignIn />
+            </Route>
+            <Route path={ROUTES.BROWSE} >
+               <Browse />
+            </Route>
+            <Route path={ROUTES.SIGN_UP} >
+               <SignUp />
+            </Route> */}
+         </Switch>
+      </Router>
+   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import {render} from 'react-dom';
 import {GlobalStyles} from "./global-styles";
 import 'normalize.css';
 import App from './App';
+import {HashRouter} from 'react-router-dom';
 import { firebase } from './lib/firebase.prod';
 import {FirebaseContext} from './context/firebase';
 
 
 render(
     <>
+        <HashRouter>
         <FirebaseContext.Provider value={{ firebase}}>
             <GlobalStyles/> 
             <App />
         </FirebaseContext.Provider>
+        </HashRouter>
     </>,
     document.getElementById('root')
 );
