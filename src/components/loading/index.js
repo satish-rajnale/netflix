83932841bbc4cfff06d15150130dbc053fb14121
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:19:33 +0530

    loading index

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,14 +1,14 @@
-import { LockBody, Spinner, Picture, ReleaseBody} from './styles/loading';
+import { LockBody, Spinner, Picture, ReleaseBody } from "./styles/loading";
 
-export default function Loading({ src, ...restProps}){
-    return (
-        <Spinner>
-            <LockBody/>
-            <Picture src={`/images/users/${src}.png`}/>
-        </Spinner>
-    )
+export default function Loading({ src, ...restProps }) {
+  return (
+    <Spinner>
+      <LockBody />
+      <Picture src={`/images/users/${src}.png`} />
+    </Spinner>
+  );
 }
 
-Loading.ReleaseBody = function LoadingReleasebody(){
-    return <ReleaseBody/>
-}
+Loading.ReleaseBody = function LoadingReleasebody() {
+  return <ReleaseBody />;
+};
