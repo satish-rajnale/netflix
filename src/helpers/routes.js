f4ebabb6f77615bc505b550f552a26e3283a25d5
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:10:25 +0530

    routes

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,52 +1,46 @@
-import { Route, Redirect} from 'react-router-dom';
+import { Route, Redirect } from "react-router-dom";
 
-export function IsUserRedirect({ user, loggedInPath, children, ...rest}){
-    return (
-        <Route
-            {...rest}
-            render={() => {
-                if(user.user == null){
-                   
-                    return children;
-                }
-                else if(user){
-                    
-
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: loggedInPath
-                            }}
-                            />
-                    )
-                }
-                return null;
-            }}
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (user.user == null) {
+          return children;
+        } else if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
             />
-    )
+          );
+        }
+        return null;
+      }}
+    />
+  );
 }
 
-export function ProtectedRoute({ user, children, ...rest}){
-    return(
-        <Route
-            {...rest}
-            render={({location}) => {
-               
-                 if(user.user == null){
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: '/',
-                                state: { from : location}
-                            }}
-                        />
-                      )
-                }
-               else if(user){
-                    return children;
-                }
-                return null;
-            }}
+export function ProtectedRoute({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user.user == null) {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { from: location },
+              }}
             />
-    )
+          );
+        } else if (user) {
+          return children;
+        }
+        return null;
+      }}
+    />
+  );
 }
