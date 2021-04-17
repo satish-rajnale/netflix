06ba10ab1ccf5328487f00a4a85384fdcb554fe1
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:06:22 +0530

    browse code prettify

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,112 +1,118 @@
 import { SelectProfileContainer } from "./profiles";
-import { FirebaseContext } from '../context/firebase';
+import { FirebaseContext } from "../context/firebase";
 import { useContext, useState, useEffect } from "react";
-import { Player, Card, Loading, Header } from '../components';
-import * as  ROUTES from '../constants/routes';
-import logo from '../logo.svg';
-import { FooterContainer } from './footer';
-import Fuse from 'fuse.js';
-
-
+import { Player, Card, Loading, Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
+import { FooterContainer } from "./footer";
+import Fuse from "fuse.js";
 
 export function BrowseContainer({ slides }) {
-    const { firebase } = useContext(FirebaseContext);
-    const [user, setUser] = useState([]);
-    const [profile, setProfile] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [category, setCategory] = useState('films');
-    const [slideRows, setSlideRows] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("films");
+  const [slideRows, setSlideRows] = useState([]);
 
-   
-    
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 3000);
-    }, [profile.displayName])
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
 
-    useEffect(() => {
-        setSlideRows(slides[category]);
-      if(firebase.auth().currentUser){
-        setUser(firebase.auth().currentUser)
-      }else{
-        setUser([]);
-      }
-    }, [slides, category, firebase]);
+  useEffect(() => {
+    setSlideRows(slides[category]);
+    if (firebase.auth().currentUser) {
+      setUser(firebase.auth().currentUser);
+    } else {
+      setUser([]);
+    }
+  }, [slides, category, firebase]);
 
-    useEffect(() => {
-        const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
-        const results = fuse.search(searchTerm).map(({ item }) => item);
-    
-        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
-          setSlideRows(results);
-        } else {
-          setSlideRows(slides[category]);
-        }
-      }, [searchTerm]);
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
 
-      function signmeOut(){
-        firebase.auth().signOut();
-        
-      }
-      
-    return profile.displayName ?
-        (<>
-            {loading ?
-                (<Loading src={user.photoURL} />) : (<Loading.ReleaseBody />)}
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
-                <Header src="joker1" dontShowOnSmallViewPort>
-            
-                <Header.Frame>
-                    <Header.Group>
-                        <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-                        <Header.TextLink
-                            active={category ==="series" ? 'true' : "false"}
-                            onClick={() => setCategory('series')}
-                          >Series</Header.TextLink>
-                        <Header.TextLink
-                            active={category ==="films" ? 'true' : "false"}
-                            onClick={() => setCategory('films')}
-                          >Films</Header.TextLink>
-                    </Header.Group>
-                    <Header.Group>
-                        <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                        <Header.Profile>
-                            <Header.Picture src={user.photoURL} />
-                            <Header.Dropdown>
-                                <Header.Group>
-                                    
-                                    <Header.TextLink>{user.displayName}</Header.TextLink>
-                                </Header.Group>
-                                <Header.Group>
-                                    <Header.TextLink onClick={() => signmeOut()} >Sign Out</Header.TextLink>
-                                </Header.Group>
-                            </Header.Dropdown>
-                        </Header.Profile>
-                    </Header.Group>
-                </Header.Frame>
-            
-            
-                <Header.Feature>
-                    <Header.Text>
-                        Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-                        City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-                        futile attempt to feel like he's part of the world around him.
-                    </Header.Text>
-                    <Header.PlayButton>Play</Header.PlayButton>
-                </Header.Feature>
-            </Header>
+  function signmeOut() {
+    firebase.auth().signOut();
+  }
 
-            <Card.Group>
-            {slideRows.map((slideItem) => (
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink
+              active={category === "series" ? "true" : "false"}
+              onClick={() => setCategory("series")}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === "films" ? "true" : "false"}
+              onClick={() => setCategory("films")}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => signmeOut()}>
+                    Sign Out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
+
+      <Card.Group>
+        {slideRows.map((slideItem) => (
           <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
             <Card.Title>{slideItem.title}</Card.Title>
-            
+
             <Card.Entities>
               {slideItem.data.map((item) => (
                 <Card.Item key={item.docId} item={item}>
-                  <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
                   <Card.Meta>
                     <Card.SubTitle>{item.title}</Card.SubTitle>
                     <Card.Text>{item.description}</Card.Text>
@@ -114,17 +120,18 @@ export function BrowseContainer({ slides }) {
                 </Card.Item>
               ))}
             </Card.Entities>
-                        <Card.Feature category={category}>
-                            <Player>
-                                <Player.Button />
-                                <Player.Video src="/videos/bunny.mp4"/>
-                            </Player>
-                        </Card.Feature>
-                    </Card>
-                ))}
-            </Card.Group>
+            <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player>
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
       <FooterContainer />
-
-        </>)
-        : (<SelectProfileContainer user={user} setProfile={setProfile} />);
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
