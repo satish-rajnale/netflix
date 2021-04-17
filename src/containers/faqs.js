058301111bd280743a8454fb6ed84807e7f789f4
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:06:50 +0530

    faqs

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,27 +1,27 @@
+import faqsData from "../fixtures/faqs.json";
+import { Accordion, OptForm } from "../components";
 
-import faqsData from '../fixtures/faqs.json';
-import { Accordion, OptForm} from '../components';
-
-export function FaqsContainer(){
-    return (
-        <Accordion>
-            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            <Accordion.Item> {faqsData.map((item) => (
-                <Accordion.Item key={item.id}>
-                    <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.body}</Accordion.Body>
-                </Accordion.Item>
-            ))}
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      <Accordion.Item>
+        {" "}
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
           </Accordion.Item>
+        ))}
+      </Accordion.Item>
 
-          <OptForm>
-             
-              <OptForm.Text>
-              Ready to watch? Enter your email to create or restart your membership.
-              </OptForm.Text>
-              <OptForm.Input placeholder="Email Address"/>
-              <OptForm.Button>Get Started</OptForm.Button>
-          </OptForm>
-        </Accordion>
-    )
+      <OptForm>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Get Started</OptForm.Button>
+      </OptForm>
+    </Accordion>
+  );
 }
