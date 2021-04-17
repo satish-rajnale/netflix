a3af1125b39e8976c877f2ea0adfa3b83e653969
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:12:38 +0530

    pages browse

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,17 +1,12 @@
-
-import { BrowseContainer } from '../containers/browse';
-import { useContent} from '../hooks'
-import  selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from "../containers/browse";
+import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
-   
-    const { series} = useContent('series');
-    const { films} = useContent('films');
+  const { series } = useContent("series");
+  const { films } = useContent("films");
 
-    const slides = selectionFilter({series, films});
-   
-    return <BrowseContainer slides={slides}/>
+  const slides = selectionFilter({ series, films });
 
+  return <BrowseContainer slides={slides} />;
 }
-
-
