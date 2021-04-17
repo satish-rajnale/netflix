1a01cf411d7ffd0c3a14ed29394b9d05a4ed3865
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:13:10 +0530

    pages index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export {default as Home} from './home';
+export { default as Home } from "./home";
 
-export {default as Browse} from './browse';
+export { default as Browse } from "./browse";
 
-export {default as SignIn} from './signin';
+export { default as SignIn } from "./signin";
 
-export {default as SignUp} from './signup';
+export { default as SignUp } from "./signup";
