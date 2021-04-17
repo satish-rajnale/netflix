57f6ca24c18a5741690ad32e36120491a97b56a5
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:19:03 +0530

    opt form index

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -1,23 +1,22 @@
+import { Container, Input, Button, Text } from "./styles/opt-form";
 
-import { Container, Input, Button, Text} from './styles/opt-form';
-
-
-export default function OptForm({children, ...restProps}){
-    return <Container {...restProps}>{children}</Container>
-} 
-
-OptForm.Input = function OptFormInput({ ...restProps}){
-    return <Input { ...restProps}/>
+export default function OptForm({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
 }
 
-OptForm.Button = function OptFormButton({children, ...restProps}){
-    return (
-        <Button { ...restProps}>
-            {children}<img src="/images/icons/chevron-right.png" alt="Try now"/>
-        </Button>
-    )
-}
+OptForm.Input = function OptFormInput({ ...restProps }) {
+  return <Input {...restProps} />;
+};
 
-OptForm.Text = function OptFormText({children, ...restProps}){
-    return <Text { ...restProps}>{children}</Text>
-}
+OptForm.Button = function OptFormButton({ children, ...restProps }) {
+  return (
+    <Button {...restProps}>
+      {children}
+      <img src="/images/icons/chevron-right.png" alt="Try now" />
+    </Button>
+  );
+};
+
+OptForm.Text = function OptFormText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
