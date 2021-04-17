4b93b6d574be404a7ed015e4afe410228840730c
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:22:52 +0530

    form index

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,37 +1,47 @@
-import { Container, Base, Error, Title, Text, TextSmall, Link, Input, Submit} from './styles/form';
+import {
+  Container,
+  Base,
+  Error,
+  Title,
+  Text,
+  TextSmall,
+  Link,
+  Input,
+  Submit,
+} from "./styles/form";
 
-export default function Form({ children, ...restProps}) {
-    return <Container {...restProps}>{children}</Container>
+export default function Form({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
 }
 
- Form.Base = function FormBase({ children, ...restProps}) {
-     return <Base {...restProps}>{children}</Base>
- }
+Form.Base = function FormBase({ children, ...restProps }) {
+  return <Base {...restProps}>{children}</Base>;
+};
 
- Form.Error = function FormError({ children, ...restProps}) {
-    return <Error {...restProps}>{children}</Error>
-}
+Form.Error = function FormError({ children, ...restProps }) {
+  return <Error {...restProps}>{children}</Error>;
+};
 
-Form.Title = function FormTitle({ children, ...restProps}) {
-    return <Title {...restProps}>{children}</Title>
-}
+Form.Title = function FormTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
 
-Form.Text = function FormText({ children, ...restProps}) {
-    return <Text {...restProps}>{children}</Text>
-}
+Form.Text = function FormText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
 
-Form.TextSmall = function FormTextSmall({ children, ...restProps}) {
-    return <TextSmall {...restProps}>{children}</TextSmall>
-}
+Form.TextSmall = function FormTextSmall({ children, ...restProps }) {
+  return <TextSmall {...restProps}>{children}</TextSmall>;
+};
 
-Form.Link = function FormLink({ children, ...restProps}) {
-    return <Link {...restProps}>{children}</Link>
-}
+Form.Link = function FormLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
 
-Form.Submit = function FormSubmit({ children, ...restProps}) {
-    return <Submit {...restProps}>{children}</Submit>
-}
+Form.Submit = function FormSubmit({ children, ...restProps }) {
+  return <Submit {...restProps}>{children}</Submit>;
+};
 
-Form.Input = function FormInput({ children, ...restProps}) {
-    return <Input {...restProps}>{children}</Input>
-}
+Form.Input = function FormInput({ children, ...restProps }) {
+  return <Input {...restProps}>{children}</Input>;
+};
