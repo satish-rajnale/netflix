e9ea6e32925e2da9e013f13e55169a37b0441f55
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:11:08 +0530

    useAuthListener

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,24 +1,24 @@
-import { useState, useEffect, useContext} from 'react';
-import { FirebaseContext} from '../context/firebase';
+import { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
 
-export default function useAuthListener(){
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
-    const {firebase} = useContext(FirebaseContext);
+export default function useAuthListener() {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("authUser"))
+  );
+  const { firebase } = useContext(FirebaseContext);
 
-    useEffect(() => {
-       
-        const listener = firebase.auth().onAuthStateChanged((authUser) => {
-            
-          if(authUser){
-                localStorage.setItem('authUser', JSON.stringify(authUser));
-                setUser(authUser);
-            }else {
-                localStorage.removeItem('authUser');
-                setUser(null);
-            }
-        });
-        return ()=> listener();
-    }, []);
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        localStorage.setItem("authUser", JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        localStorage.removeItem("authUser");
+        setUser(null);
+      }
+    });
+    return () => listener();
+  }, []);
 
-    return { user};
+  return { user };
 }
