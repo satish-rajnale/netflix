257cf66ad70ac62163526af942598ce1fb9963a8
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 11:15:00 +0530

    src undex

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {GlobalStyles} from "./global-styles";
-import 'normalize.css';
-import App from './App';
-import {HashRouter} from 'react-router-dom';
-import { firebase } from './lib/firebase.prod';
-import {FirebaseContext} from './context/firebase';
-
+import React from "react";
+import { render } from "react-dom";
+import { GlobalStyles } from "./global-styles";
+import "normalize.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 render(
-    <React.StrictMode>
-        <HashRouter>
-        <FirebaseContext.Provider value={{ firebase}}>
-            <GlobalStyles/> 
-            <App />
-        </FirebaseContext.Provider>
-        </HashRouter>
-        </React.StrictMode>
-    ,
-    document.getElementById('root')
+  <React.StrictMode>
+    <HashRouter>
+      <FirebaseContext.Provider value={{ firebase }}>
+        <GlobalStyles />
+        <App />
+      </FirebaseContext.Provider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
