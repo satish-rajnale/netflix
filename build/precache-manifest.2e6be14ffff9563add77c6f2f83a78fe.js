d068c274ed251f1496c4e1ef1e4b67f0a5e0664f
satish-rajnale <satishrajnale98@gmail.com>
2021-05-03 21:50:04 +0530

    branch change

---
+++ build/precache-manifest.2e6be14ffff9563add77c6f2f83a78fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e20d88088238b28b303e8ecffa3f01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f5109278b96e0df38a5",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6f5109278b96e0df38a5",
+    "url": "/static/js/2.1fd254b4.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.1fd254b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83a9a8dcd312c08fa28",
+    "url": "/static/js/main.781da2ab.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  }
+]);
